63791aff160a9040a68dc8cc18d3b84ef70eebe7
Nishadsarkar23 <168167240+Nishadsarkar23@users.noreply.github.com>
2024-04-30 11:57:23 +0530

    Update __main__.py

--- DAXXMUSIC/__main__.py
+++ DAXXMUSIC/__main__.py
@@ -50,7 +50,7 @@ async def init():
         pass
     await DAXX.decorators()
     LOGGER("DAXXMUSIC").info(
-        "✦ 𝗠𝗔𝗗𝗘 𝗕𝗬 𝗥𝗢𝗬-𝗘𝗗𝗜𝗧𝗫...♥︎"
+        "✦ 𝗠𝗔𝗗𝗘 𝗕𝗬 KARTIK S...♥︎"
     )
     await idle()
     await app.stop()
